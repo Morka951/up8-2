18d7e0884aa8effbcd0676f1882e4f18acc2979e
AetherSky-arch <74071029+AetherSky-arch@users.noreply.github.com>
2023-01-25 12:02:12 +0100

    Added directory for game engines class
    Initially contains pong demo project

---
+++ Godot/pong/Player1.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+
+var speed : Vector2
+var velocity : Vector2
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	speed = Vector2(250,250)
+	velocity = Vector2(0,0)
+
+func get_input():
+	velocity = Vector2(0,0)
+	if Input.is_action_pressed("ui_up"):
+		velocity=Vector2(0,-1) * speed
+	if Input.is_action_pressed("ui_down"):
+		velocity=Vector2(0,1) * speed
+
+func _physics_process(_delta):
+	get_input()
+	velocity = move_and_slide(velocity)
---
+++ Godot/pong/ball.gd
@@ -0,0 +1,40 @@
+extends KinematicBody2D
+
+
+var speed : Vector2
+var velocity : Vector2
+var points1 : int
+var points2 : int
+
+func _ready():
+	randomize()
+	speed = Vector2(300,300)
+	velocity = Vector2(rand_range(-6,6),rand_range(-6,6))
+	points1 = 0
+	points2 = 0
+
+onready var pos1 = get_node("/root/Main/Player1").position
+onready var pos2 = get_node("/root/Main/Player2").position
+
+func _physics_process(_delta):
+	if move_and_collide(velocity) != null:
+		velocity = velocity.bounce(move_and_collide(velocity).normal)
+	if global_position.x > pos1.x:
+		points(1)
+		reset()
+	if global_position.x < pos2.x:
+		points(2)
+		reset()
+
+func reset():
+	global_position = Vector2(424,207)
+	randomize()
+	velocity = Vector2(rand_range(-6,6),rand_range(-6,6))
+
+func points(player):
+	if player == 1:
+		points1 = points1 + 1
+		get_node("/root/Main/label1").text = str(points1)
+	else:
+		points2 = points2 + 1
+		get_node("/root/Main/label2").text = str(points2)
